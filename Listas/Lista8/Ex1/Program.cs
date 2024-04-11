40326c2a83c7225967cafcf91bb76197b9efb643
guihhramos <guiramos902@gmail.com>
2024-04-11 19:28:59 -0300

    lista 8

---
+++ Listas/Lista8/Ex1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+struct Pizza
+{
+    public int id;
+    public string sabor;
+    public double preco;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pizza pizza1;
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        System.Console.WriteLine("Id:");
+        Console.ResetColor();
+        pizza1.id = Convert.ToInt32(Console.ReadLine());
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        System.Console.WriteLine("Sabor:");
+        Console.ResetColor();
+        pizza1.sabor = Console.ReadLine();
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        System.Console.WriteLine("Preco:");
+        Console.ResetColor();
+        pizza1.preco = double.Parse(Console.ReadLine());
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        System.Console.WriteLine(pizza1.id);
+        System.Console.WriteLine(pizza1.sabor);
+        System.Console.WriteLine(pizza1.preco);
+        Console.ResetColor();
+    }
+}
---
+++ Listas/Lista8/Ex2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+struct Produto
+{
+    public int id;
+    public string nome;
+    public double preco;
+    public bool disponivelEmEstoque;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int TAMANHO_VETOR = 4;
+        Produto[] Produtos = new Produto[TAMANHO_VETOR];
+        int contadoor = 0;
+
+        for (int contador = 0; contador < Produtos.Length; contador++)
+        {
+            Produtos[contador].id = Convert.ToInt32(Console.ReadLine());
+            Produtos[contador].nome = Console.ReadLine();
+            Produtos[contador].preco = double.Parse(Console.ReadLine());
+            Produtos[contador].disponivelEmEstoque = bool.Parse(Console.ReadLine());
+        }
+
+        for(int contador = 0; contador < Produtos.Length; contador++)
+        {
+             if (Produtos[contador].disponivelEmEstoque == true)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                System.Console.WriteLine("\n" + Produtos[contador].nome);
+                System.Console.WriteLine($"{Math.Round(Produtos[contador].preco, 2)}");
+                Console.ResetColor();
+            }
+        }
+    }
+}
